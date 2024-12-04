7fe0672dd1610761b3945bf611e13ea6a00147bc
MykytaChystyi <neyton61@gmail.com>
2024-12-04 16:02:10 +0200

    Оновив алгоритм шифрування і дешифрування

--- Core/ProfileCondition.cs
+++ Core/ProfileCondition.cs
@@ -33,8 +33,9 @@ namespace Core
         {
             byte[] salt;
             byte[] buffer2;
-            
-            using (var bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8)) {
+
+            using (var bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8, HashAlgorithmName.SHA256))
+            {
                 salt = bytes.Salt;
                 buffer2 = bytes.GetBytes(0x20);
             }
@@ -62,7 +63,7 @@ namespace Core
             hashedBuffer = new byte[0x20];
             Buffer.BlockCopy(src, 0x11, hashedBuffer, 0, 0x20);
 
-            using (var bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            using (var bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8, HashAlgorithmName.SHA256))
             {
                 buffer = bytes.GetBytes(0x20);
             }
@@ -94,15 +95,21 @@ namespace Core
         public string Encrypt(string clearText)
         {
             byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
-            using (var encryptor = Aes.Create()) 
+            using (var encryptor = Aes.Create())
             {
-                var pdb = new Rfc2898DeriveBytes(sum_names,  new byte[] 
-                { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-                using (var ms = new MemoryStream()) 
+                var pdb = new Rfc2898DeriveBytes(
+                    sum_names,
+                    new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 },
+                    100_000, 
+                    HashAlgorithmName.SHA256 
+                );
+
+                encryptor.Key = pdb.GetBytes(32); 
+                encryptor.IV = pdb.GetBytes(16);  
+
+                using (var ms = new MemoryStream())
                 {
-                    using (var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write)) 
+                    using (var cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
                     {
                         cs.Write(clearBytes, 0, clearBytes.Length);
                         cs.Close();
@@ -112,20 +119,27 @@ namespace Core
             }
             return clearText;
         }
+
         public string Decrypt(string cipherText)
         {
             cipherText = cipherText.Replace(" ", "+");
             byte[] cipherBytes = Convert.FromBase64String(cipherText);
-            using (var encryptor = Aes.Create()) 
+
+            using (var encryptor = Aes.Create())
             {
-                var pdb = new Rfc2898DeriveBytes(sum_names, new byte[] 
-                { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-                using (MemoryStream ms = new MemoryStream()) 
+                var pdb = new Rfc2898DeriveBytes(
+                    sum_names,
+                    new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 },
+                    100_000,
+                    HashAlgorithmName.SHA256 
+                );
+
+                encryptor.Key = pdb.GetBytes(32); 
+                encryptor.IV = pdb.GetBytes(16); 
+
+                using (var ms = new MemoryStream())
                 {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), 
-                        CryptoStreamMode.Write)) 
+                    using (var cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
                     {
                         cs.Write(cipherBytes, 0, cipherBytes.Length);
                         cs.Close();
@@ -135,5 +149,6 @@ namespace Core
             }
             return cipherText;
         }
+
     }
 }
